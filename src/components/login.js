5bd1671ebc2ecca3a294b18b4ac3c5cccfc5bde6
Vanilla2 <solmarians05@gmail.com>
2020-07-28 23:12:51 +0300

    Fixed Login Page

--- src/components/login.js
+++ src/components/login.js
@@ -5,13 +5,22 @@ import {Link} from "react-router-dom";
 export default (props) => {
     const [email,setEmail] = useState("");
     const [password,setPassword] = useState("");
+    const handleLogIn = async () => {
+        try {
+            await firebase.auth().signInWithEmailAndPassword(email, password);
+            props.history.push('/');
+        }
+        catch(e){
+            alert(e.message);
+        }
+    }
     return(
         <>
             <h2>Log In</h2>
             <h2>{props.logged ? "You are logged in" : "You are not logged in"}</h2>
             <input placeholder="email" name = "email" onChange={(e) => setEmail(e.target.value)}/>
             <input placeholder="password" name = "password" onChange={(e) => setPassword(e.target.value)}/>
-            <button onClick={() => firebase.auth().signInWithEmailAndPassword(email, password)}>Log In</button>
+            <button onClick={handleLogIn}>Log In</button>
             <Link to = "/signup">Sign up</Link>
         </>
     )
