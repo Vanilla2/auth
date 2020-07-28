876d06e5b9d28565f68c256f69f511780a5e2273
Vanilla2 <solmarians05@gmail.com>
2020-07-28 23:08:37 +0300

    Updates

---
+++ precache-manifest.6c0c85ec922dd4ae316765625b831d48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69e5e903501683d25c90d0eeb50b0e32",
+    "url": "/auth/index.html"
+  },
+  {
+    "revision": "948759a1cbadaf72674d",
+    "url": "/auth/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "977ff148875933d00588",
+    "url": "/auth/static/js/2.1fb7a13e.chunk.js"
+  },
+  {
+    "revision": "4224b32d50aca7d0b4f248bf0384ed4f",
+    "url": "/auth/static/js/2.1fb7a13e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "948759a1cbadaf72674d",
+    "url": "/auth/static/js/main.e5fdc69e.chunk.js"
+  },
+  {
+    "revision": "2364c5eba16329a7e817",
+    "url": "/auth/static/js/runtime-main.9b47b971.js"
+  }
+]);
